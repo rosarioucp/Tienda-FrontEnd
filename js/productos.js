e075397549867bf156750f48f191db8631a51540
Rosario <rosariogauto20@gmail.com>
2026-04-30 15:43:31 -0300

    agrego los juegos con su spropiedades

---
+++ js/productos.js
@@ -0,0 +1,50 @@
+const juegos = [
+    {
+        id: 1,
+        nombre: "FIFA 23",
+        precio: 13,
+        descripcion: "Simulador de futbol",
+        categoria: "Deporte",
+        imagen: "https://placehold.co/200x150"
+    },
+    {
+        id: 2,
+        nombre: "Call of Duty: Modern Warfare III ",
+        precio: 59.99,
+        descripcion: "Guerra moderna/Disparos",
+        categoria: "Acción",
+        imagen: "https://placehold.co/200x150"
+    },
+    {
+        id: 3,
+        nombre: "Minecraft Legends",
+        precio: 23.99,
+        descripcion: "Construcción, exploración y supervivencia",
+        categoria: "Aventura",
+        imagen: "https://placehold.co/200x150"
+    },
+    {
+        id: 4,
+        nombre: "R.E.P.O",
+        precio: 4.49,
+        descripcion: "Terror, Supervivencia, Cooperativo.",
+        categoria: "Terror",
+        imagen: "https://placehold.co/200x150"
+    },
+    {
+        id: 5,
+        nombre: "Counter Strike 2",
+        precio: 14.59,
+        descripcion: "Terror, Supervivencia, Cooperativo.",
+        categoria: "Acción",
+        imagen: "https://placehold.co/200x150"
+    },
+    {
+        id: 6,
+        nombre: "Grand Theft Auto V",
+        precio: 44.99,
+        descripcion: "Mundo abierto, acción",
+        categoria: "Acción, aventura, carrera",
+        imagen: "https://placehold.co/200x150"
+    }
+]
