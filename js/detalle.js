2ec95ad140f7d0556812708f24821ff2cd50c686
Rosario <rosariogauto20@gmail.com>
2026-05-04 14:59:26 -0300

    detalles de los juegos

---
+++ js/detalle.js
@@ -0,0 +1,11 @@
+const juego = JSON.parse(localStorage.getItem('juegoSeleccionado'))
+
+document.getElementById('juego-imagen').src = juego.imagen
+document.getElementById('juego-imagen').alt = juego.nombre
+document.getElementById('juego-nombre').textContent = juego.nombre
+document.getElementById('juego-descripcion').textContent = juego.descripcion
+document.getElementById('juego-precio').textContent = '$' + juego.precio
+
+function irACheckout() {
+    window.location.href = 'checkout.html'
+}
