4d2d8af9e1ab31d51a017b982b1794f0f691df96
Rosario <rosariogauto20@gmail.com>
2026-05-06 12:12:48 -0300

    Agregue una descripcion mas larga a la segunda pantalla

--- js/detalle.js
+++ js/detalle.js
@@ -4,6 +4,7 @@ document.getElementById('juego-imagen').src = juego.imagen
 document.getElementById('juego-imagen').alt = juego.nombre
 document.getElementById('juego-nombre').textContent = juego.nombre
 document.getElementById('juego-descripcion').textContent = juego.descripcion
+document.getElementById('juego-descripcionLarga').textContent = juego.descripcionLarga
 document.getElementById('juego-precio').textContent = '$' + juego.precio
 
 function irACheckout() {
